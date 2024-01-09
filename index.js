e66712631bc0cd0f3d61ba6dc5e5b8879b2ee82e
Charleen Wang <charleen23wang@gmail.com>
2024-01-09 12:07:53 -0800

    js: implemented storage of note data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var noteElements = document.getElementsByClassName('note');
 
 var handleClickEvent = function(e) {
-    element = e.target;
+    var element = e.target;
     element.contentEditable = true;
     element.focus();
 
@@ -9,13 +9,37 @@ var handleClickEvent = function(e) {
 } 
 
 var handleBlurEvent = function(e) {
-    element = e.target;    
+    var element = e.target;
+    element.contentEditable = false;
+
+    saveNotes();
 }
 
-for (var i = 0; i < noteElements.length; i++) {
-    var element = noteElements[i];
-    element.addEventListener('click', handleClickEvent);
+var saveNotes = function() {
+    var noteData = [];
+    for (var i = 0; i < noteElements.length; i++) {
+        var element = noteElements[i];
+        noteData[i] = element.innerHTML;
+    }
+    console.log(noteData);
+    // save to local
+    var store = { 'notes': noteData }
+    chrome.storage.local.set(store).then(() => {
+        console.log("Value is set");
+        console.log(store);
+    });
 }
 
+chrome.storage.local.get(["notes"], function(data) {
+    var notesData = data.notes;
+    for (var i = 0; i < noteElements.length; i++) {
+        console.log(notesData[i])
+        var element = noteElements[i];
+        element.innerHTML = notesData[i];
+        
+
+        element.addEventListener('click', handleClickEvent);
+    }
+});
 
 
